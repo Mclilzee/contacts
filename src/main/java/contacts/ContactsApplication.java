692567211a0d6ab73c586dfee9a130fd4c1913d5
Emad Ali <mc.lilzee@hotmail.com>
2022-11-19 16:53:43 +0100

    Add contact creator

--- src/main/java/contacts/ContactsApplication.java
+++ src/main/java/contacts/ContactsApplication.java
@@ -1,5 +1,6 @@
 package contacts;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContactsApplication implements CommandLineRunner {
 
+    private final CreateContacts createContacts;
+
+    public ContactsApplication(CreateContacts createContacts) {
+        this.createContacts = createContacts;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ContactsApplication.class, args);
     }
 
+
+
     @Override
     public void run(String... args) throws Exception {
-
+        createContacts.createContacts();
     }
 }
---
+++ src/main/java/contacts/CreateContacts.java
@@ -0,0 +1,12 @@
+package contacts;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateContacts {
+
+    public void createContacts() {
+
+    }
+
+}
