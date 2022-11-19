c2fcc10a4e5daa04dc04ed3b09e1c1ae3203aecf
Emad Ali <mc.lilzee@hotmail.com>
2022-11-19 16:43:28 +0100

    Init

---
+++ src/main/java/contacts/ContactsApplication.java
@@ -0,0 +1,18 @@
+package contacts;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/test/java/contacts/ContactsApplicationTests.java
@@ -0,0 +1,12 @@
+package contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
