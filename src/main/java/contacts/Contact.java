e76a3de815178f6d3eda8c324855b77ce6040941
Emad Ali <mc.lilzee@hotmail.com>
2022-11-21 15:19:33 +0100

    Fix regex missing element

--- src/main/java/contacts/Contact.java
+++ src/main/java/contacts/Contact.java
@@ -13,7 +13,7 @@ public class Contact {
     static {
         String firstGroupWrapped = "(\\([\\da-z]+\\)[\\s-][\\da-z]{2,})";
         String secondGroupWrapped = "([\\da-z]+[\\s-]\\([\\da-z]{2,}\\))";
-        String bothGroupsUnwrapped = "([\\d-z]+[\\s-][\\da-z]{2,})";
+        String bothGroupsUnwrapped = "([\\da-z]+[\\s-][\\da-z]{2,})";
         String fullRegex =  String.format("\\+?(%s|%s|%s)([\\s-][\\da-z]+)+", firstGroupWrapped, secondGroupWrapped, bothGroupsUnwrapped);
         pattern = Pattern.compile(fullRegex, Pattern.CASE_INSENSITIVE);
     }
