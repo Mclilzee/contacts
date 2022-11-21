68e04f0ab7962211a012709d5fb146899e669de6
Emad Ali <mc.lilzee@hotmail.com>
2022-11-21 05:44:53 +0100

    Add record adding options

--- src/main/java/contacts/options/ContactOption.java
+++ src/main/java/contacts/options/ContactOption.java
@@ -1,5 +1,6 @@
 package contacts.options;
 
+import contacts.Contact;
 import contacts.PhoneBook;
 import org.springframework.stereotype.Component;
 
@@ -22,17 +23,33 @@ public class ContactOption {
                case "exit":
                    return;
                case "count":
-                   printContactsCount();
+                   printRecordsCount();
+                   break;
+               case "add":
+                   addNewContact();
                    break;
            }
        }
     }
 
-    private void printContactsCount() {
-        System.out.println("The Phone book has " + phoneBook.getContacts().size() + " records.");
+    private void printRecordsCount() {
+        System.out.printf("The Phone Book has %d records.%n", phoneBook.getContacts().size());
     }
 
     private void printInstructions() {
         System.out.print("Enter action (add, remove, edit, count, list, exit): ");
+    }
+
+    private void addNewContact() {
+        System.out.print("Enter the name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter the surname: ");
+        String surname = scanner.nextLine();
+
+        System.out.print("Enter the number: ");
+        String number = scanner.nextLine();
+
+        phoneBook.addContact(new Contact(name, surname, number));
     }
 }
