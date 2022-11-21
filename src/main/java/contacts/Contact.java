9a65c317ab8bb91896da727552821afd9e487909
Emad Ali <mc.lilzee@hotmail.com>
2022-11-21 02:51:25 +0100

    Remove uneccessary util class

--- src/main/java/contacts/Contact.java
+++ src/main/java/contacts/Contact.java
@@ -1,20 +1,33 @@
 package contacts;
 
-import contacts.utils.StringUtils;
 import lombok.Getter;
+
+import java.util.regex.Pattern;
 
 @Getter
 public class Contact {
+
+    private static final Pattern pattern;
+
+    static {
+        String firstGroupWrapped = "(\\([\\da-z]+\\)[\\s-][\\da-z]{2,})";
+        String secondGroupWrapped = "([\\da-z]+[\\s-]\\([\\da-z]{2,}\\))";
+        String bothGroupsUnwrapped = "([\\d-z]+[\\s-]\\[\\da-z]{2,})";
+        String fullRegex =  String.format("\\+?(%s|%s|%s)([\\s-][\\da-z])+", firstGroupWrapped, secondGroupWrapped, bothGroupsUnwrapped);
+        pattern = Pattern.compile(fullRegex, Pattern.CASE_INSENSITIVE);
+    }
 
     private final String name;
     private final String surname;
     private String phoneNumber;
 
+
+
     public Contact(String name, String surname, String phoneNumber) {
         this.name = name;
         this.surname = surname;
 
-        if (StringUtils.isValidPhoneNumber(phoneNumber)) {
+        if (isValidPhoneNumber(phoneNumber)) {
             this.phoneNumber = phoneNumber;
         } else {
             this.phoneNumber = "[no number]";
@@ -23,10 +36,14 @@ public class Contact {
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        if (StringUtils.isValidPhoneNumber(phoneNumber)) {
+        if (isValidPhoneNumber(phoneNumber)) {
             this.phoneNumber = phoneNumber;
         } else {
             System.out.println("Wrong phone number!");
         }
+    }
+
+    public static boolean isValidPhoneNumber(String phoneNumber) {
+        return pattern.matcher(phoneNumber).matches();
     }
 }
