efc6b27e469f5c5e8dd28bb0f004fe21dc41589d
Emad Ali <mc.lilzee@hotmail.com>
2022-11-21 03:22:39 +0100

    Fix regex phone checking

--- src/main/java/contacts/Contact.java
+++ src/main/java/contacts/Contact.java
@@ -13,8 +13,8 @@ public class Contact {
     static {
         String firstGroupWrapped = "(\\([\\da-z]+\\)[\\s-][\\da-z]{2,})";
         String secondGroupWrapped = "([\\da-z]+[\\s-]\\([\\da-z]{2,}\\))";
-        String bothGroupsUnwrapped = "([\\d-z]+[\\s-]\\[\\da-z]{2,})";
-        String fullRegex =  String.format("\\+?(%s|%s|%s)([\\s-][\\da-z])+", firstGroupWrapped, secondGroupWrapped, bothGroupsUnwrapped);
+        String bothGroupsUnwrapped = "([\\d-z]+[\\s-][\\da-z]{2,})";
+        String fullRegex =  String.format("\\+?(%s|%s|%s)([\\s-][\\da-z]+)+", firstGroupWrapped, secondGroupWrapped, bothGroupsUnwrapped);
         pattern = Pattern.compile(fullRegex, Pattern.CASE_INSENSITIVE);
     }
 
--- src/test/java/contacts/ContactTest.java
+++ src/test/java/contacts/ContactTest.java
@@ -1,6 +1,7 @@
 package contacts;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -16,7 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ContactTest {
 
     private final String WRONG_NUMBER_OUTPUT = "Wrong number format!\r\n";
-    private final Contact contact = new Contact("John", "Doe", "+0 (123) 456-789-ABcd");
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
     @BeforeEach
@@ -26,23 +26,36 @@ class ContactTest {
 
     @Test
     void getName() {
+        Contact contact = new Contact("John", "Doe", "+0 (123) 456-789-ABcd");
         String expected = "John";
         assertEquals(expected, contact.getName());
     }
 
     @Test
     void getSurname() {
+        Contact contact = new Contact("John", "Doe", "+0 (123) 456-789-ABcd");
         String expected = "Doe";
         assertEquals(expected, contact.getSurname());
     }
 
-    @Test
-    void correctPhoneNumber() {
-        String expected = "+0 (123) 456-789-ABcd";
-        assertEquals(expected, contact.getPhoneNumber());
+    @ParameterizedTest
+    @DisplayName("Correct phone number set correctly")
+    @MethodSource("provideCorrectPhoneNumbers")
+    void correctPhoneNumber(String number) {
+        Contact contact = new Contact("John", "Doe", number);
+        assertEquals(number, contact.getPhoneNumber());
     }
 
     @ParameterizedTest
+    @DisplayName("Correct phone number outputs no message to the console")
+    @MethodSource("provideCorrectPhoneNumbers")
+    void correctPhoneNumberOutputNoMessage(String number) {
+        Contact contact = new Contact("John", "Doe", number);
+        assertEquals("", outputStreamCaptor.toString());
+    }
+
+    @ParameterizedTest
+    @DisplayName("Wrong number input will set field to correct value")
     @MethodSource("provideWrongNumbers")
     void wrongPhoneNumber(String number) {
         Contact contact = new Contact("John", "Doe", number);
@@ -52,6 +65,7 @@ class ContactTest {
 
 
     @ParameterizedTest
+    @DisplayName("Wrong number init will output the correct message")
     @MethodSource("provideWrongNumbers")
     void testInitOutput(String number) {
         Contact contact = new Contact("John", "Doe", number);
@@ -59,11 +73,21 @@ class ContactTest {
     }
 
     @ParameterizedTest
+    @DisplayName("Wrong number in setter will not change value")
+    @MethodSource("provideWrongNumbers")
+    void setterWrongPhoneNumber(String number) {
+        Contact contact = new Contact("John", "Doe", "+0 (123) 456-789-ABcd");
+        contact.setPhoneNumber(number);
+        assertEquals("+0 (123) 456-789-ABcd", contact.getPhoneNumber());
+    }
+
+    @ParameterizedTest
+    @DisplayName("Wrong number in setter will output correct message")
     @MethodSource("provideWrongNumbers")
     void setterWrongPhoneNumberOutput(String number) {
+        Contact contact = new Contact("John", "Doe", "+0 (123) 456-789-ABcd");
         contact.setPhoneNumber(number);
         assertEquals(WRONG_NUMBER_OUTPUT, outputStreamCaptor.toString());
-        assertEquals("+0 (123) 456-789-ABcd", contact.getPhoneNumber());
     }
 
     private static Stream<String> provideWrongNumbers() {
@@ -73,6 +97,13 @@ class ContactTest {
                 "+(0) (123) 123-123-123",
                 "+123 0 123-123-321",
                 "+123 (123)1231-123-123"
+        );
+    }
+
+    private static Stream<String> provideCorrectPhoneNumbers() {
+        return Stream.of(
+                "+0 (123) 456-789-ABcd",
+                "(123) 234 345-456"
         );
     }
 }
