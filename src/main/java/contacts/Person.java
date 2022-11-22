2cbd4e374fccaff9c6f7a00327fbc200fc2901bd
Emad Ali <mc.lilzee@hotmail.com>
2022-11-23 00:54:33 +0100

    Create person class

---
+++ src/main/java/contacts/Person.java
@@ -0,0 +1,11 @@
+package contacts;
+
+public class Person {
+    private String name;
+    private String surname;
+    private String gender;
+    private String birthDate;
+
+    public Person(String name, String surname, String gender, String birthDate) {
+    }
+}
